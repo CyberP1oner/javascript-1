b94bae928d83420efd587cd8571b8e3b40ed92bf
Alexander Iurkin <iurkinalexander@gmail.com>
2025-01-16 16:02:00 +0300

    Арифметические операторы

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log('Hello world');
+// Базовые арифметические операторы
+
+const width = 10;
+const height = 5;
+const space = width * height;
+const newWeight = width - 4;
+const newWeighе2 = width + 4;
+const divesion = newWeight / newWeighе2;
+const volume = 2 ** 3; // 2 * 2 * 2
+console.log(volume);
+
+// Строки 
+const city = 'Москва';
+const street = 'Новослободская';
+console.log(city + ', ' + street);
